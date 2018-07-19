73eff2c94924901ec0d5e6f67a68123d2dca51a5
yonggoonoh <nohyg90@gmail.com>
2018-07-20 00:14:02 +0900

    class 소개

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-interface Person {
-    name: string;
-    age: number;
+class Person {
+    public name: string;
+    public age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
 }
 
 const sayHi = (person: Person): string => {
     return `Hello ${person.name}(${person.age})`
 }
 
-const person: Person = {
-    name: "noh",
-    age: 1
-};
+const person = new Person("noh", 1);
 
 console.log(sayHi(person));
