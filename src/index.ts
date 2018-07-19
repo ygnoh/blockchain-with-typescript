5b72e203d0f9c88ceb1888c47cd0125ce207f353
yonggoonoh <nohyg90@gmail.com>
2018-07-20 00:11:18 +0900

    interface 소개: interface는 js로 컴파일 되지 않는다

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello");
+interface Person {
+    name: string;
+    age: number;
+}
+
+const sayHi = (person: Person): string => {
+    return `Hello ${person.name}(${person.age})`
+}
+
+const person: Person = {
+    name: "noh",
+    age: 1
+};
+
+console.log(sayHi(person));
